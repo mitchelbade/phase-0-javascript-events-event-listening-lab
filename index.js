834e52880ff485c6e84b5a10b75c345eede4e25d
mitchelbade <mitchelm.bade@gmail.com>
2022-01-06 16:00:40 -0800

    commit tree 2e324e6564e0e37d7fa0eed546a9d6329a9d9d7b

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+  const input = document.getElementById('input');
+
+  const clickAlert = clickAlert => alert('I was clicked!');
+  
+  input.addEventListener('click', clickAlert);
 }
+
